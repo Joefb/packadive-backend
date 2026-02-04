0a66c9491f8532abd62a0c9c2fe60f73469dfdfb
Joe Burgess <joeezmail@gmail.com>
2026-02-03 23:12:31 -0800

    Add admin_auth_token_required in auth.py

--- app/util/auth.py
+++ app/util/auth.py
@@ -44,3 +44,32 @@ def auth_token_required(f):
         return f(*args, **kwargs)
 
     return decoration
+
+
+def admin_auth_token_required(f):  # f stands for the function that is getting wrapped
+    @wraps(f)
+    def decoration(
+        *args, **kwargs
+    ):  # The function that runs before the functiuon that we're wrapping
+        token = None
+
+        if "Authorization" in request.headers:
+            token = request.headers["Authorization"].split()[1]
+
+        if not token:
+            return jsonify({"error": "token missing from authorization headers"}), 401
+
+        try:
+            data = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+            if data.get("user_name") != "admin":
+                return jsonify({"message": "admin access required"}), 403
+            print(data)
+            request.logged_in_id = data["sub"]
+        except jose.exceptions.ExpiredSignatureError:
+            return jsonify({"message": "token is expired"}), 403
+        except jose.exceptions.JWTError:
+            return jsonify({"message": "invalid token"}), 401
+
+        return f(*args, **kwargs)
+
+    return decoration
