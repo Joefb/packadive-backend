8f0eeacd5568fb81a65a515bf13087fe1d843d9d
Joe Burgess <joeezmail@gmail.com>
2026-02-18 17:55:55 -0800

    Change apps to app

--- planadive.py
+++ planadive.py
@@ -2,7 +2,7 @@ from app.models import db
 from app import create_app
 
 ## Add when deploying
-apps = create_app("ProductionConfig")
-with apps.app_context():
+app = create_app("ProductionConfig")
+with app.app_context():
     # db.drop_all()
     db.create_all()
